f8ccc3abb63cc83b771fe7f0a8ce890ddc3a778d
Nick <nick.ruider@gmail.com>
2022-04-01 16:24:12 +0200

    added forgotten unversioned file + music module

---
+++ src/music/music.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from "@nestjs/mongoose";
+
+import { Song, SongSchema } from "./core/schemas/song.schema";
+import { Playlist, PlaylistSchema } from "./core/schemas/playlist.schema";
+import { SongController } from "./song/song.controller";
+import { PlaylistController } from "./playlist/playlist.controller";
+import { SongService } from "./song/song.service";
+import { PlaylistService } from "./playlist/playlist.service";
+import { FirebaseService } from "../firebase-storage/firebase.service";
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([{ name: Song.name, schema: SongSchema}]),
+        MongooseModule.forFeature([{ name: Playlist.name, schema: PlaylistSchema}]),
+    ],
+    controllers: [
+        SongController,
+        PlaylistController
+    ],
+    providers: [
+        SongService,
+        PlaylistService,
+        FirebaseService
+    ]
+})
+export class MusicModule {}
